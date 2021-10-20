f0c616e6414b8eb21074e685461c768c31c0ce78
Amar Boodhun <adarshboodhun3@gmail.com>
2021-10-20 16:13:03 +0400

    first commit

---
+++ week2/script.js
@@ -0,0 +1,147 @@
+//variable
+// var message;
+// message = 10;
+
+var message = "hello";
+var user = "zivuch";
+var email = "zivuch@gmail.com"
+
+meassage = "bye";
+
+console.log(message, user, email);
+
+
+let classname = "hello";
+let b = "zivuch";
+let c = "zivuch@gmail.com";
+
+const COLOR_RED = "red";
+
+
+
+//message = "bye";
+
+//console.log(message, user, email);
+
+
+
+
+let admin;
+let name;
+
+//let admin, name;
+//name ='john';
+
+
+//javascript types 
+/*
+number 
+string
+boolean
+undefind
+null
+*/
+let a = null
+
+
+let und;
+
+
+let isgreater = true;
+let isless = false;
+isless = true;
+
+let num1 = 10;
+let num2 = 5;
+console.log(num1 / num2);
+
+let mes1 = 'hello';
+let user1 = "Jhon";
+console.log(mes1 + user);
+
+
+//alert
+alert('inccorect password');
+
+//prompt
+let result = prompt('please rom 1 to 10', '9');
+console.log(result);
+
+console.log("hello")
+
+
+
+
+//conversion
+
+let num = 10;
+ num2 = 5;
+console.log(num + num); 15
+
+let numtxt = String(num);
+let numtxt2= String(num2);
+console.log(numtxt + numtxt2); 105
+
+num = String(num); "10"|
+console.log(typeof num);
+num2 =String(num2); "5"
+console.log(num + num2);
+
+
+let num3 = "10";
+num3 = Number(num3);
+
+Boolean(0)//false
+Boolean(1)//true
+
+
+1 =="a"
+
+
+
+
+//Array
+let arr = [10,5,20];
+console.log( arr[0] ); //10
+console.log(arr[2]);
+
+let =["Apple","Orange","Plum"];
+
+let arr3 = ["apple",5,true];
+
+
+
+let arr4 = [30,
+["Apple","Orange"]
+
+];
+// console.log(arr4[1][1]);
+
+var fruit = ["Banana", "Orange", "Apple", "Mango"];
+
+console.log
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
